5a0943ff430babaf23aa55622796de683a9eea40
Goraf <22941576+Goraf@users.noreply.github.com>
2018-05-03 15:01:37 +0200

    Add encryption algorithm (Caesar cipher)

--- project-4/main.js
+++ project-4/main.js
@@ -1,6 +1,30 @@
-var appModule = (function(window) {
+var appModule = (function (window) {
+  // Caesar cipher
+  function encryptMsg() {
+    var msg = window.prompt("Write a message you want to encrypt:");
+    var msgCharsCodes = [];
+    var charOffset = 3;
+    var alphabetRange = "Z".charCodeAt() - "A".charCodeAt();
+    for (var i = 0; i < msg.length; i++) {
+      msgCharsCodes[i] = msg.charCodeAt(i);
+      var isLowerLatin = msgCharsCodes[i] >= "a".charCodeAt() && msgCharsCodes[i] <= "z".charCodeAt();
+      var isUpperLatin = msgCharsCodes[i] >= "A".charCodeAt() && msgCharsCodes[i] <= "Z".charCodeAt();
+
+      if (isLowerLatin || isUpperLatin) {
+        msgCharsCodes[i] += charOffset;
+        if ((isLowerLatin && msgCharsCodes[i] > "z".charCodeAt()) || (isUpperLatin && msgCharsCodes[i] > "Z".charCodeAt())) {
+          msgCharsCodes[i] -= alphabetRange + 1;
+        }
+      }
+
+      msgCharsCodes[i] = String.fromCharCode(msgCharsCodes[i]);
+    }
+    msg = msgCharsCodes.join("");
+    window.alert("Encrypted message:\n\n" + msg);
+  }
+
   function run() {
-    console.log("works");
+    encryptMsg();
   }
 
   return {
