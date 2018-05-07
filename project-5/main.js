c70218323c8e915e07c89e67af5c8e227e76323c
Goraf <22941576+Goraf@users.noreply.github.com>
2018-05-07 21:43:30 +0200

    Add function to construct todo items ...
    * adds test items
    * adds items on submit

--- project-5/main.js
+++ project-5/main.js
@@ -1,5 +1,29 @@
 var appModule = (function (window) {
+  function addTodoItem(e) {
+    e.preventDefault();
+    var todoList = window.document.getElementsByClassName("todo-items-container")[0];
+
+    var todoItem = window.document.createElement("article");
+    todoItem.classList.add("todo-item");
+
+    var title = window.document.createElement("h2");
+    title.classList.add("todo-item__title");
+    title.textContent = "Test todo item";
+
+    var removeBtn = window.document.createElement("button");
+    removeBtn.classList.add("todo-item__remove-button");
+    removeBtn.textContent = "Remove";
+
+    todoItem.appendChild(title);
+    todoItem.appendChild(removeBtn);
+
+    todoList.appendChild(todoItem);
+  }
+  
   function run() {
+    var form = window.document.getElementById("todo-form");
+    form.addEventListener("submit", addTodoItem);
+    //addTodoItem();
   }
 
   return {
