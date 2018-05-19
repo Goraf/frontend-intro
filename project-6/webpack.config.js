3c2a8a690a31d305019c9c6eab51666775699beb
Goraf <22941576+Goraf@users.noreply.github.com>
2018-05-19 20:22:42 +0200

    Add HtmlWebpackPlugin ...
    configure to:
    * use existing index.html as template
    * place bundle.js in header

--- project-6/webpack.config.js
+++ project-6/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "none",
@@ -20,5 +21,12 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      inject: "head"
+    })
+  ]
 }
