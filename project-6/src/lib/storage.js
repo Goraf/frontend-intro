a3b33e289f9703cecabda0a16b42cd65a6130f39
Goraf <22941576+Goraf@users.noreply.github.com>
2018-05-20 21:39:01 +0200

    Add simple documentation to 'storage' module

--- project-6/src/lib/storage.js
+++ project-6/src/lib/storage.js
@@ -1,5 +1,9 @@
 // Provides function to save and load from localStorage
 
+/**
+ * Loads data from localStorage
+ * @param {string} key 
+ */
 function loadData(key) {
   if (typeof key !== "string") {
     console.warn("loadData: wrong type of parameter");
@@ -16,6 +20,11 @@ function loadData(key) {
   }
 }
 
+/**
+ * Saves data to localStorage
+ * @param {string} key 
+ * @param {*} value 
+ */
 function saveData(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
 }
